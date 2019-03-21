d530d5c22248758e8b4e4d4e11dfc0883b15d789
shankar <shankar@sankar>
2019-03-21 23:32:28 +0530

    add the list and lenth

--- scala_eclipse_test/src/singleten_class.scala
+++ scala_eclipse_test/src/singleten_class.scala
@@ -1,8 +1,10 @@
 
-
 object singleten_class {
    def mymethod(): Unit = {
     printf("Hello World")
+    val int_list=List(1,2,3,4)
+    println()
+    printf("Length of list is ="+int_list.length)
   }
   
   def main(args: Array[String]): Unit = {
