f28799a0fe588319dccb40ee9aeb4d335c093123
shankar <shankar@sankar>
2019-03-21 23:59:57 +0530

    added the list_buffer and updated the list_buffer by mutable object

--- scala_eclipse_test/src/singleten_class.scala
+++ scala_eclipse_test/src/singleten_class.scala
@@ -1,10 +1,20 @@
+import scala.collection.mutable.ListBuffer
+
 
 object singleten_class {
    def mymethod(): Unit = {
     printf("Hello World")
     val int_list=List(1,2,3,4)
-    println()
-    println("Length of list is ="+int_list.length)
+    int_list.foreach(x=>println(x+1))
+    val int_list_buf=ListBuffer(6,7,8,9)
+    println("Adding the value 10 to list_buffer")
+    int_list_buf+=10
+    int_list_buf.foreach(println)
+    println("delete the item 5 from list_buffer")
+    int_list_buf-=5
+    int_list_buf.foreach(println)
+     
+    println("Length of list is="+int_list.length)
   }
   
   def main(args: Array[String]): Unit = {
