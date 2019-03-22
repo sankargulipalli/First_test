d3170f32756df0ac77f20d9f3dfe10d6e5e15b5b
shankar <shankar@sankar>
2019-03-23 00:26:49 +0530

    add the set collection

--- scala_eclipse_test/src/singleten_class.scala
+++ scala_eclipse_test/src/singleten_class.scala
@@ -13,6 +13,9 @@ object singleten_class {
     println("delete the item 5 from list_buffer")
     int_list_buf-=5
     int_list_buf.foreach(println)
+    val int_set=Set(12,45,45,7,8,89,89,62)
+    println("we have added the new set to eleminated the duplicated values")
+    int_set.foreach(println)
      
     println("Length of list is="+int_list.length)
   }
