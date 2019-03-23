01a8be96e4c447f4582d43b8e955c3774b8067f1
shankar <shankar@sankar>
2019-03-24 00:04:43 +0530

    Add the Empty list and get the user input from console

--- scala_eclipse_test/src/read_input_console.scala
+++ scala_eclipse_test/src/read_input_console.scala
@@ -1,10 +1,23 @@
 object read_input
 {
   def read_method():Unit = {
-    println("Enter the input value:")
+    //println("Enter the input value:")
     
-    val input= scala.io.StdIn.readLine()
-    println(input)
+    val n= scala.io.StdIn.readLine("Please enter the nuber for n: ").toInt
+    var i=1
+    var color_list= scala.collection.mutable.ListBuffer.empty[Int]
+    
+    //color_list+=5
+    //color_list+=7
+    //println(color_list)
+    while(i<=n)
+    {
+    color_list += scala.io.StdIn.readLine("Please enter the colors number: ").toInt
+    i+=1
+   
+    }
+    printf("color_list:"+ color_list)
+    
   }
    def main(args: Array[String]): Unit = {
     read_method()
